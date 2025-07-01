18bba414959bb32926e8b96ed9d126009571c3af
Vijay Pant <vijaypant486@gmail.com>
2025-07-01 23:38:18 +0530

    Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerRB = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
         
     }
 }
