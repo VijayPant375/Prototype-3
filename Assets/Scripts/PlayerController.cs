84e56963d2e9428e03f4968669c24cb60bd0908e
Vijay Pant <vijaypant486@gmail.com>
2025-07-04 22:49:06 +0530

    3.1 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     public float jumpForce = 10;
     public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
-        
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
